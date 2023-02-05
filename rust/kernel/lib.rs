2cd3fcff8bdb9c825e66e0d33f37a288c2321744
Asahi Lina <lina@asahilina.net>
2023-02-05 20:42:15 +0900

    rust: drm: ioctl: Add DRM ioctl abstraction
    DRM drivers need to be able to declare which driver-specific ioctls they
    support. Add an abstraction implementing the required types and a helper
    macro to generate the ioctl definition inside the DRM driver.
    
    Note that this macro is not usable until further bits of the abstraction
    are in place (but it will not fail to compile on its own, if not called).
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -45,6 +45,8 @@ pub mod device;
 pub mod device_id;
 pub mod devres;
 pub mod driver;
+#[cfg(CONFIG_DRM = "y")]
+pub mod drm;
 pub mod error;
 pub mod faux;
 #[cfg(CONFIG_RUST_FW_LOADER_ABSTRACTIONS)]
