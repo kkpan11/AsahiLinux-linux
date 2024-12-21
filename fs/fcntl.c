herrnst/linux-asahi

HACK: Lol libwebrtc
Remove this in a year or two, hopefully the Chromium/CEF ecosystem of
fail will have caught up with their libwebrtc version by then.

Signed-off-by: Hector Martin <marcan@marcan.st>

fs/fcntl.c
@@ -325,6 +325,8 @@ static long do_fcntl(int fd, unsigned int cmd, unsigned long arg,
 		err = f_dupfd(arg, filp, 0);
 		break;
 	case F_DUPFD_CLOEXEC:
+		if (arg >= 1024)
+			arg = 0; /* Lol libwebrtc */
 		err = f_dupfd(arg, filp, O_CLOEXEC);
 		break;
 	case F_GETFD:

kkpan11/AsahiLinux-linux

fs/fcntl.c
@@ -454,6 +454,8 @@ static long do_fcntl(int fd, unsigned int cmd, unsigned long arg,
 		err = f_dupfd(argi, filp, 0);
 		break;
 	case F_DUPFD_CLOEXEC:
+		if (arg >= 1024)
+			argi = 0; /* Lol libwebrtc */
 		err = f_dupfd(argi, filp, O_CLOEXEC);
 		break;
 	case F_DUPFD_QUERY: