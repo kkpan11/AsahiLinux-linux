mikewyxtt/linux

ASoC: tas2764: Enable some Apple quirks by default
0xf seems to fix the random overcurrent behavior.

Signed-off-by: Hector Martin <marcan@marcan.st>

sound/soc/codecs/tas2764.c
@@ -50,7 +50,7 @@ struct tas2764_priv {
 	bool unmuted;
 };
 
-static int apple_quirks;
+static int apple_quirks = 0xf;
 module_param(apple_quirks, int, 0644);
 MODULE_PARM_DESC(apple_quirks, "Mask of quirks to mimic after Apple's SN012776 driver");
 

kkpan11/AsahiLinux-linux

sound/soc/codecs/tas2764.c
@@ -48,7 +48,7 @@ struct tas2764_priv {
 	bool unmuted;
 };
 
-static int apple_quirks;
+static int apple_quirks = 0x3f;
 module_param(apple_quirks, int, 0644);
 MODULE_PARM_DESC(apple_quirks, "Mask of quirks to mimic after Apple's SN012776 driver");
 