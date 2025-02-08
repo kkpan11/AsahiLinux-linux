mikewyxtt/linux

fixup! mux: apple dp crossbar: Support t602x DP cross bar variant

drivers/mux/apple-display-crossbar.c
@@ -423,7 +423,7 @@ static const struct apple_dpxbar_hw apple_dpxbar_hw_t6000 = {
 	.ops = &apple_dpxbar_ops,
 };
 
-const static struct apple_dpxbar_hw apple_dpxbar_hw_t6020 = {
+static const struct apple_dpxbar_hw apple_dpxbar_hw_t6020 = {
 	.n_ufp = 9,
 	.ops = &apple_dpxbar_t602x_ops,
 };

kkpan11/AsahiLinux-linux

drivers/mux/apple-display-crossbar.c
@@ -405,7 +405,7 @@ static int apple_dpxbar_probe(struct platform_device *pdev)
 	return 0;
 }
 
-const static struct apple_dpxbar_hw apple_dpxbar_hw_t8103 = {
+static const struct apple_dpxbar_hw apple_dpxbar_hw_t8103 = {
 	.n_ufp = 2,
 	.tunable = 0,
 	.ops = &apple_dpxbar_ops,