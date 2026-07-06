cc348e87a989e2c8403c4b01d5a4496d5d1c3227
Gary Guo <gary@garyguo.net>
2026-07-06 13:44:19 +0100

    rust: io: rename `Mmio` to `MmioOwned`
    Most users would more commonly reach out to a view of `Mmio` rather than an
    owned instance of `Mmio`. Only implementor of `Io` like `Bar` or `IoMem`
    would need the owned version. Thus, rename `Mmio` to `MmioOwned` so that
    the name `Mmio` can be used for the view type instead.
    
    Reviewed-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Suggested-by: Danilo Krummrich <dakr@kernel.org>
    Link: https://rust-for-linux.zulipchat.com/#narrow/channel/288089-General/topic/Generic.20I.2FO.20backends/near/571198078
    Link: https://patch.msgid.link/20260706-io_projection-v6-6-72cd5d055d54@garyguo.net
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/kernel/devres.rs
+++ rust/kernel/devres.rs
@@ -69,7 +69,7 @@ struct Inner<T> {
 ///     devres::Devres,
 ///     io::{
 ///         Io,
-///         Mmio,
+///         MmioOwned,
 ///         MmioRaw,
 ///         PhysAddr,
 ///         Region, //
@@ -106,11 +106,11 @@ struct Inner<T> {
 /// }
 ///
 /// impl<const SIZE: usize> Deref for IoMem<SIZE> {
-///    type Target = Mmio<SIZE>;
+///    type Target = MmioOwned<SIZE>;
 ///
 ///    fn deref(&self) -> &Self::Target {
 ///         // SAFETY: The memory range stored in `self` has been properly mapped in `Self::new`.
-///         unsafe { Mmio::from_raw(&self.0) }
+///         unsafe { MmioOwned::from_raw(&self.0) }
 ///    }
 /// }
 /// # fn no_run(dev: &Device<Bound>) -> Result<(), Error> {
