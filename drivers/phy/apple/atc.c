6045f5abdc90f5dca00d5ac82e10c484eb5809bf
Sven Peter <sven@kernel.org>
2026-08-20 20:37:25 +0200

    phy: apple: atc: Support DUMMY PIPEHANDLER state in configure_pipehandler
    For both Thunderbolt and DisplayPort atcphy_configure_pipehandler is
    reached with a request to switch to the DUMMY state (i.e. usb2 only).
    With the current code this breaks USB2 when all four SS lanes are used
    for DisplayPort AltMode because the -EINVAL is passed all the way back
    to the phy_set_mode() call which results in tearing down xhci and dwc3
    again.
    Let's actually handle that case correctly and also drop the default from
    the switch such that we get a compiler warning if another pipehandler
    state is ever added and forgotten here.
    
    Reported-by: Paul Cristian <p4ulcristian@gmail.com>
    Closes: https://github.com/AsahiLinux/linux/pull/515
    Fixes: 8e98ca1e74db ("phy: apple: Add Apple Type-C PHY")
    Cc: stable@vger.kernel.org
    Signed-off-by: Sven Peter <sven@kernel.org>

--- drivers/phy/apple/atc.c
+++ drivers/phy/apple/atc.c
@@ -1245,7 +1245,7 @@ static int atcphy_configure_pipehandler_dummy(struct apple_atcphy *atcphy)
 
 static int atcphy_configure_pipehandler(struct apple_atcphy *atcphy, bool host)
 {
-	int ret;
+	int ret = -EINVAL;
 
 	lockdep_assert_held(&atcphy->lock);
 
@@ -1260,8 +1260,10 @@ static int atcphy_configure_pipehandler(struct apple_atcphy *atcphy, bool host)
 		ret = atcphy_configure_pipehandler_dummy(atcphy);
 		atcphy->pipehandler_up = false;
 		break;
-	default:
-		ret = -EINVAL;
+	case ATCPHY_PIPEHANDLER_STATE_DUMMY:
+		ret = atcphy_configure_pipehandler_dummy(atcphy);
+		atcphy->pipehandler_up = false;
+		break;
 	}
 
 	return ret;
