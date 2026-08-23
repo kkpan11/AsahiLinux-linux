chadmed/linux

WIP: drm: apple: Add 14.7 FW ABI
Asahi Linux will target the firmware ABI from macOS 14.8.3
for i/mBoot-loaded firmware on M3 series machines. This
version identifies itself as v14.7, so add it as such to
the DCP driver.

Most things are working, except for EDID retrieval for external
displays and audio over HDMI/DisplayPort. There is also an issue
where DCP will try to read destroyed/invisible framebuffers for
some reason or another, causing IOVA faults. We will need to
figure out how to get the new firmware to clear surfaces and
stop reading the nonexistent framebuffers at the very least before
shipping.

Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

drivers/gpu/drm/apple/iomfb.c
@@ -433,6 +433,9 @@ int dcp_crtc_atomic_modeset(struct drm_crtc *crtc,
 	case DCP_FIRMWARE_V_13_5:
 		ret = iomfb_modeset_v13_3(dcp, crtc_state);
 		break;
+	case DCP_FIRMWARE_V_14_7:
+		ret = iomfb_modeset_v14_7_0(dcp, crtc_state);
+		break;
 	default:
 		WARN_ONCE(true, "Unexpected firmware version: %u\n",
 			  dcp->fw_compat);
@@ -479,6 +482,9 @@ void dcp_flush(struct drm_crtc *crtc, struct drm_atomic_state *state)
 	case DCP_FIRMWARE_V_13_5:
 		iomfb_flush_v13_3(dcp, crtc, state);
 		break;
+	case DCP_FIRMWARE_V_14_7:
+		iomfb_flush_v14_7_0(dcp, crtc, state);
+		break;
 	default:
 		WARN_ONCE(true, "Unexpected firmware version: %u\n", dcp->fw_compat);
 		break;
@@ -490,6 +496,9 @@ static void iomfb_start(struct apple_dcp *dcp)
 	switch (dcp->fw_compat) {
 	case DCP_FIRMWARE_V_13_5:
 		iomfb_start_v13_3(dcp);
+		break;
+	case DCP_FIRMWARE_V_14_7:
+		iomfb_start_v14_7_0(dcp);
 		break;
 	default:
 		WARN_ONCE(true, "Unexpected firmware version: %u\n", dcp->fw_compat);
@@ -538,6 +547,9 @@ void iomfb_shutdown(struct apple_dcp *dcp)
 	switch (dcp->fw_compat) {
 	case DCP_FIRMWARE_V_13_5:
 		iomfb_shutdown_v13_3(dcp);
+		break;
+	case DCP_FIRMWARE_V_14_7:
+		iomfb_shutdown_v14_7_0(dcp);
 		break;
 	default:
 		WARN_ONCE(true, "Unexpected firmware version: %u\n", dcp->fw_compat);

kkpan11/AsahiLinux-linux

drivers/gpu/drm/apple/iomfb.c
@@ -436,6 +436,9 @@ int dcp_crtc_atomic_modeset(struct drm_crtc *crtc,
 	case DCP_FIRMWARE_V_13_5:
 		ret = iomfb_modeset_v13_3(dcp, crtc_state);
 		break;
+	case DCP_FIRMWARE_V_14_7:
+		ret = iomfb_modeset_v14_7_0(dcp, crtc_state);
+		break;
 	default:
 		WARN_ONCE(true, "Unexpected firmware version: %u\n",
 			  dcp->fw_compat);
@@ -485,6 +488,9 @@ void dcp_flush(struct drm_crtc *crtc, struct drm_atomic_state *state)
 	case DCP_FIRMWARE_V_13_5:
 		iomfb_flush_v13_3(dcp, crtc, state);
 		break;
+	case DCP_FIRMWARE_V_14_7:
+		iomfb_flush_v14_7_0(dcp, crtc, commit);
+		break;
 	default:
 		WARN_ONCE(true, "Unexpected firmware version: %u\n", dcp->fw_compat);
 		break;
@@ -499,6 +505,9 @@ static void iomfb_start(struct apple_dcp *dcp)
 		break;
 	case DCP_FIRMWARE_V_13_5:
 		iomfb_start_v13_3(dcp);
+		break;
+	case DCP_FIRMWARE_V_14_7:
+		iomfb_start_v14_7_0(dcp);
 		break;
 	default:
 		WARN_ONCE(true, "Unexpected firmware version: %u\n", dcp->fw_compat);
@@ -550,6 +559,9 @@ void iomfb_shutdown(struct apple_dcp *dcp)
 		break;
 	case DCP_FIRMWARE_V_13_5:
 		iomfb_shutdown_v13_3(dcp);
+		break;
+	case DCP_FIRMWARE_V_14_7:
+		iomfb_shutdown_v14_7_0(dcp);
 		break;
 	default:
 		WARN_ONCE(true, "Unexpected firmware version: %u\n", dcp->fw_compat);